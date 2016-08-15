3640a3b7f41ac3cbb416009b65e6fc6abfa860f7
jianwei <jianwei@outlook.com>
2016-08-15 10:12:52 +0800

    optimize Book

--- src/main/java/lambdasinaction/appa/Book.java
+++ src/main/java/lambdasinaction/appa/Book.java
@@ -9,7 +9,10 @@ public class Book {
 
     public static void main(String[] args) {
         Author[] authors = Book.class.getAnnotationsByType(Author.class);
-        Arrays.asList(authors).stream().forEach(a -> {
+//        Arrays.asList(authors).stream().forEach(a -> {
+//            System.out.println(a.name());
+//        });
+        Arrays.stream(authors).forEach(a -> {
             System.out.println(a.name());
         });
     }
